4db867341608986eccbcf25792803ba18239f966
Skye Book <skye.book@gmail.com>
2014-10-07 12:05:25 -0400

    Add configuration files and set up host seeding

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,17 @@
 default[:elasticsearch] = {
+  :cluster => {
+    :name => "elasticsearch"
+  },
   :version => '1.3.4'
 }
+
+# Populate an array with the addresses of other instances in the ES layer
+seed_array = []
+
+node["opsworks"]["layers"]["elasticsearch"]["instances"].each do |instance_name, values|
+  seed_array << values["private_ip"]
+end
+
+hosts_string = "[" + seed_array.map!{|host| "\"#{host}\""}.join(", ") + "]"
+  
+set[:elasticsearch][:discovery][:zen][:ping][:unicast][:hosts] = seed_array
