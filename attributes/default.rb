af36510b87eeb2c4e0e23ee15e4ffb0f47b7ce9a
Skye Book <skye.book@gmail.com>
2014-10-07 15:01:11 -0400

    Fix seed host population

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,8 +8,12 @@ default[:elasticsearch] = {
 # Populate an array with the addresses of other instances in the ES layer
 seed_array = []
 
+# Include this instance in the seed list
+seed_array << node["opsworks"]["instance"]["private_ip"]
+
 # This is kind of hacky, but it reliably gets the OpsWorks layer we are running on (presuming the instance only belongs to a single layer)
-node["opsworks"]["instance"]["layers"][0]["instances"].each do |instance_name, values|
+layer = node["opsworks"]["instance"]["layers"][0]
+node["opsworks"]["layers"][layer]["instances"].each do |instance_name, values|
   seed_array << values["private_ip"]
 end
 
