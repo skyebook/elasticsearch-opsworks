d074b3dbdbc74a5decf0170d7fb63c3b8ea2f4de
Skye Book <skye.book@gmail.com>
2014-10-08 14:36:59 -0400

    Use default hash when creating HTPasswd entry

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,5 +26,5 @@ hosts_string = "[" + seed_array.map!{|host| "\"#{host}\""}.join(", ") + "]"
 set[:elasticsearch][:discovery][:zen][:ping][:unicast][:hosts] = seed_array
 
 # Create HTPasswd
-command = "printf \""+node[:elasticsearch][:auth][:username]+":$(openssl passwd -apr1 "+node[:elasticsearch][:auth][:passwd]+")\""
+command = "printf \""+default[:elasticsearch][:auth][:username]+":$(openssl passwd -apr1 "+default[:elasticsearch][:auth][:passwd]+")\""
 set[:elasticsearch][:auth][:htpasswd] = `#{command}`
